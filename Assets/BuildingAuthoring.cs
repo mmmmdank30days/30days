e32a40dc110fa0efe5d306791a317974b58a6456
mmmmdank30days <houseofhighgrade@gmail.com>
2025-08-02 22:19:08 -0400

    d

---
+++ Assets/BuildingAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class BuildingAuthoring : MonoBehaviour
+{
+    public float health = 100f;
+
+    class Baker : Baker<BuildingAuthoring>
+    {
+        public override void Bake(BuildingAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Renderable);
+            AddComponent(entity, new BuildingTag());
+            AddComponent(entity, new BuildingHealth { Value = authoring.health });
+        }
+    }
+}
+
+public struct BuildingTag : IComponentData { }
+public struct BuildingHealth : IComponentData
+{
+    public float Value;
+}
